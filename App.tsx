4da9d7b68a547ea29054d897f8ae5455037d8f14
Siyatori <Sarawut-s02@hotmail.com>
2024-07-13 11:10:11 +0700

    BasicReactNative

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import AppFooter from "./component/AppFooter";
+import AppHeader from "./component/AppHeader";
 
-export default function App() {
+export default function App(): React.JSX.Element {
+  const onClickMe = () => {
+    Alert.alert("Hi", "hello React.js");
+  };
+  const users = [
+    { id: 1001, name: "John" },
+    { id: 1002, name: "Mary" },
+  ];
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <AppHeader title="This is Header" year={2018} />
+      <AppHeader title="This is Header2" />
+      <Text>Hello React Native</Text>
+      { users.map((data,index) => {
+        return(
+          <Text key={data.id}>
+            No. {index+1} ID: {data.id} Name: {data.name}
+          </Text>
+        )
+
+      })}
+      <Button title="ClickMe" onPress={onClickMe} color={"red"} />
+      {/* <Button title='Click Me' onPress={() => {Alert.alert('Hi', 'React Native is Fun')}}></Button> */}
       <StatusBar style="auto" />
+      <AppFooter />
     </View>
   );
 }
@@ -13,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
