56ec490491723f312486deaf4e6132a8b7290e01
Siyatori <Sarawut-s02@hotmail.com>
2024-08-04 12:12:07 +0700

    Practice3_03_08_67

--- component/Login.tsx
+++ component/Login.tsx
@@ -1,15 +1,32 @@
-import { View, Text, Button, TextInput, } from 'react-native'
+import { View, Text, Button, TextInput, Alert } from 'react-native'
 import React from 'react'
 import { stylesLogin } from "../style/styles";
 
 const Login = (): React.JSX.Element => {
+
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+
+  const buttonSubmit = () => {
+    if(name == "" && email ==""){
+      Alert.alert("Alert", "Please Enter Name & Email")
+    }
+    else if(name == ""){
+      Alert.alert("Alert", "Please Enter Name")
+    }
+    else if(email == ""){
+      Alert.alert("Alert", "Please Enter Email")
+    }
+    else{
+      Alert.alert("Alert", "Success")
+    }
+  }
+
   return (
     <View style={stylesLogin.container}>
-      <TextInput style={stylesLogin.input}>Enter Name</TextInput>
-      <TextInput style={stylesLogin.input}>Enter Email</TextInput>
-      <Button title='submitBT' />
-      {/* <Text style={stylesPractice.text}>{displayFullname}</Text> */}
-      {/* <Button title="ClickMe" onPress={onButtonClick} /> */}
+      <TextInput style={stylesLogin.input} placeholder='Enter Name' value={name} onChangeText={setName}></TextInput>
+      <TextInput style={stylesLogin.input} placeholder='Enter Email' value={email} onChangeText={setEmail}></TextInput>
+      <Button title='submit' onPress={buttonSubmit} />
     </View>
   )
 }
