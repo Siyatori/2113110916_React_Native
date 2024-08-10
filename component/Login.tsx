30a8b76ddce9b697911026c470104ebaa022e774
Siyatori <Sarawut-s02@hotmail.com>
2024-08-10 10:06:07 +0700

    Practice4_TextInput_10_08_67

--- component/Login.tsx
+++ component/Login.tsx
@@ -1,34 +1,63 @@
-import { View, Text, Button, TextInput, Alert } from 'react-native'
-import React from 'react'
+import { View, Text, Button, TextInput, Alert } from "react-native";
+import React from "react";
 import { stylesLogin } from "../style/styles";
 
 const Login = (): React.JSX.Element => {
-
   const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const validateEmail = (email: string): boolean => {
+    const recheckEmail = /\S+@\S+\.\S+/;
+    return recheckEmail.test(email);
+  };
 
   const buttonSubmit = () => {
-    if(name == "" && email ==""){
-      Alert.alert("Alert", "Please Enter Name & Email")
+    let errorMessage = "";
+    if (!name) {
+      errorMessage += "Please Enter Name\n";
     }
-    else if(name == ""){
-      Alert.alert("Alert", "Please Enter Name")
+    if (!email) {
+      errorMessage += "Please Enter Email\n";
+    } else if (!validateEmail(email)) {
+      errorMessage += "Invalid Email Format\n";
     }
-    else if(email == ""){
-      Alert.alert("Alert", "Please Enter Email")
+
+    if (!password) {
+      errorMessage += "Please Enter Password\n";
+    } else if (password.length < 6) {
+      errorMessage += "Password must be at lease 6 characters\n";
     }
-    else{
-      Alert.alert("Alert", "Success")
+
+    if (errorMessage) {
+      Alert.alert("Error", errorMessage.trim(), [{ text: "OK" }]);
+      return;
     }
-  }
+    Alert.alert("Alert", "Success", [{ text: "OK" }]);
+  };
 
   return (
     <View style={stylesLogin.container}>
-      <TextInput style={stylesLogin.input} placeholder='Enter Name' value={name} onChangeText={setName}></TextInput>
-      <TextInput style={stylesLogin.input} placeholder='Enter Email' value={email} onChangeText={setEmail}></TextInput>
-      <Button title='submit' onPress={buttonSubmit} />
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Name"
+        value={name}
+        onChangeText={setName}
+      ></TextInput>
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Email"
+        value={email}
+        onChangeText={setEmail}
+      ></TextInput>
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+      ></TextInput>
+      <Button title="submit" onPress={buttonSubmit} />
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
