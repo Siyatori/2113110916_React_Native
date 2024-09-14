bb96ed63d6eda9e4a528b8defc79bf6276debe96
Siyatori <Sarawut-s02@hotmail.com>
2024-09-14 08:46:21 +0700

    Attendace_14_09_67

--- App.tsx
+++ App.tsx
@@ -1,39 +1,88 @@
-import { FlatList, StyleSheet, Text, View, Button } from "react-native";
+// Only import react-native-gesture-handler on native platforms
+import "react-native-gesture-handler";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+
 import HomeScreen from "./screen/HomeScreen";
 import AboutScreen from "./screen/AboutScreen";
-import {NavigationContainer} from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import MenuScreen from "./screen/MenuScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CreatePostScreen from "./screen/CreatePostScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { HeaderButtonsProvider } from "react-navigation-header-buttons";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import ProductScreen from "./screen/ProductScreen";
+const ProductStack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
-
-const App = (): React.JSX.Element => {
-
-  const HomeStack = createNativeStackNavigator();
-
+function HomeStackScreen() {
   return (
-    <NavigationContainer>
-      <HomeStack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerStyle:{backgroundColor:'#33beff'},
-          headerTintColor:'white',
-          headerTitleStyle:{fontWeight:'bold'},
-          headerTitleAlign: 'center',
+    <HomeStack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        // headerStyle:{backgroundColor:'#2302aa'},
+        // headerTintColor:'white',
+        headerTitleStyle: { fontWeight: "bold" },
+      }}
+    >
+      <HomeStack.Screen
+        name="Home"
+        component={HomeScreen}
+        // options={{title:'หน้าหลัก'}}
+      />
+      <HomeStack.Screen
+        name="About"
+        component={AboutScreen}
+        options={{
+          title: "เกี่ยวกับเรา",
+          headerStyle: { backgroundColor: "#2302aa" },
+          headerTintColor: "white",
+          headerTitleStyle: { fontWeight: "bold" },
+          //  headerTitleAlign:'center',
         }}
-      >
-        <HomeStack.Screen name="Home" component={HomeScreen} options={{title:"หน้าหลัก"}} />
-        <HomeStack.Screen name="About" component={AboutScreen} options={{
-          title:"เกี่ยวกับเรา", 
-          headerStyle:{backgroundColor:'#33beff'},
-          headerTintColor:'white',
-          headerTitleStyle:{fontWeight:'bold'},
-          headerTitleAlign: 'center',
-          }} />
-        <HomeStack.Screen name="CreatePost" component={CreatePostScreen} />
-      </HomeStack.Navigator>
-    </NavigationContainer>
+      />
+      {/* <HomeStack.Screen name='CreatePost' component={CreatePostScreen}/> */}
+    </HomeStack.Navigator>
+  );
+}
+function ProductStackScreen() {
+  return (
+    <ProductStack.Navigator
+      initialRouteName="Product"
+      screenOptions={{
+        // headerStyle:{backgroundColor:'#2302aa'},
+        // headerTintColor:'white',
+        headerTitleStyle: { fontWeight: "bold" },
+      }}
+    >
+      <ProductStack.Screen
+        name="Product"
+        component={ProductScreen}
+      />
+      {/* <HomeStack.Screen name='CreatePost' component={CreatePostScreen}/> */}
+    </ProductStack.Navigator>
+  );
+}
+const App = (): React.JSX.Element => {
+  return (
+    <SafeAreaProvider>
+      <HeaderButtonsProvider stackType="native">
+        <NavigationContainer>
+          <Drawer.Navigator
+            screenOptions={{ headerShown: false }}
+            drawerContent={(props) => <MenuScreen {...props} />}
+          >
+            <Drawer.Screen name="HomeStack" component={HomeStackScreen} />
+            <Drawer.Screen name="Product" component={ProductStackScreen}/>
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </HeaderButtonsProvider>
+    </SafeAreaProvider>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({});
